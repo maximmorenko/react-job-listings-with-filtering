a55817e4403bcaa210f9405bb7f317233ebd7a68
Maksym Morenko <morenko.maxim@gmail.com>
2022-08-24 17:45:32 +0300

    Реализация фильтрации списка

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -3,7 +3,8 @@
 
 // теперь данные достаем не из мока а из нашего созданого селектора с использованием useSelector
 import { useSelector, useDispatch } from 'react-redux';
-import { selectAllPositions } from 'store/positions/position-selectors';
+import { selectVisiblePositions } from 'store/positions/position-selectors';
+import { selectFilters } from 'store/filters/filter-selectors';
 
 import { addFilter } from 'store/filters/filter-actions';
 
@@ -13,10 +14,17 @@ import { JobPosition } from './JobPosition';
 // чтобы повесить экшн (событие клика) на на конкретный бедж, нужна функция
 // сделаем ее на уровне списка и передадим ниже
 
+// на уровне джоб листа нам также нужны актуальные фильтры, досанем их из селектора
 const JobList = () => {
     const dispatch = useDispatch();
-    const positions = useSelector(selectAllPositions);
-
+    const currentFilters = useSelector(selectFilters); // все актуальные фильтры
+    // const positions = useSelector(selectAllPositions); // до сих пор мы отрисовывали все позиции, 
+    // сделаем так чтобы отрисовывались только отфильтрованые, для этого возьмем наш стейт, 
+    // вывзываем селектор актуальных позиций (функцию фильтрации) и передаем в нее актуальный стейт и актуальные фильтры
+    // мы должны передать актуальные фильтры, потому что мы не можем создавать селекторы, которые получали бы любое количество параметров, 
+    // если больше одного, то вотрой передаем вручную 
+    const positions = useSelector((state) => selectVisiblePositions(state, currentFilters));
+   
     const handleAddFilter = (filter) => {
         // функция будет вызывать диспечер, диспечер будет вызывать экшн криэйтор, 
         // и в экшн передаем полученый фильтр
