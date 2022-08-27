7623cd1b75eba407c13ed72c0a2edd3b9077e7a1
Maksym Morenko <morenko.maxim@gmail.com>
2022-08-27 23:02:49 +0300

    localeStorage with persist

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
-import { store } from './store'
+import { store, persistor } from './store'
+
+// для работы персиста импортируем персистГейт (провайдер) и оборачиваем в него наше пришожение
+import { PersistGate } from 'redux-persist/integration/react'
 
 // обернем приложение в провайдер из реакт-редакс
 ReactDOM.render(
-  <React.StrictMode>
-    {/* в провайдер обязатеьно передаем стор */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        {/* в провайдер обязатеьно передаем стор */}
+        <Provider store={store}>
+            <PersistGate 
+            // PersistGate требеет ключи:
+            loading={null} //прелоадер
+            persistor={persistor}
+            >
+                <App />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,30 @@
 import { createStore } from 'redux';
 import { rootReduser } from './root-reduser'
 
+// для работы персиста ниобходимо импортировать дополнительные хелперы
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+// для работы персиста создаем конфиг с ключами
+const persistConfig = {
+    key: 'root',
+    storage,
+    // по умолчанию имеет два ключа, таже можно добавить массивы whiteList or blackList
+    whiteList: ['filters', 'positions'], //содержит список который нужно включить в локал
+    // по умолчанию все что есть в сторе бедет включено, эти списки для исключений или выборочного включения
+    // проверка (в логах aplication / locale sorige)
+    // blackList: [], //содержит список который НЕ нужно включить в локал
+    // можно использовать один, на выбор
+}
+
+// также нужно создать персист редюсер, обертка над нашим рутРедюсером
+const persistedReducer = persistReducer(persistConfig, rootReduser)
+
 export const store = createStore(
-    rootReduser, //комбаин редюсеров
+    // меняем обычный рутредусер на персистРудюсер
+    persistedReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //для запуска расширения React Developer Tools
 );
+
+// также нужно експортировать персистерСтор
+export const persistor = persistStore(store);
